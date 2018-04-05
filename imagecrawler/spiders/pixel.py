37aafbf259454d62b770294ca9379799ae073dc6
Denniskamau <denniskamau3@gmail.com>
2018-04-05 11:56:39 +0300

    Spider able to get the page

--- imagecrawler/spiders/pixel.py
+++ imagecrawler/spiders/pixel.py
@@ -1,11 +1,21 @@
 
 import re
 import scrapy
+from scrapy.linkextractor import LinkExtractor
+from scrapy.selector import Selector
+
 
 
 
 class PexelsScraper(scrapy.Spider):
     name = "pixels"
+    src_extractor = re.compile('src="([^"]*)"')
+    tags_extractor = re.compile('alt="([^"]*)"')
+    # Define the regex we'll need to filter the returned links
+    url_matcher = re.compile('^https:\/\/www\.textbookcentre\.com\/catalogue\/')
+    
+    # Create a set that'll keep track of ids we've crawled
+    crawled_ids = set()
 
 
     def start_requests(self):
@@ -14,6 +24,29 @@ class PexelsScraper(scrapy.Spider):
         
 
     def parse(self, response):
+        body = Selector(text=response.body)
+        #The class of an image from textbook center is img-responsive
+        images = body.css('img.img-responsive').extract()
+
+        # body.css().extract() returns a list which might be empty
+        for image in images:
+            img_url = PexelsScraper.src_extractor.findall(image)[0]
+            tags = [tag.replace(',', '').lower() for tag in PexelsScraper.tags_extractor.findall(image)[0].split(' ')]
+            print img_url, tags
+
+        link_extractor = LinkExtractor(allow=PexelsScraper.url_matcher)
+        next_links = [link.url for link in link_extractor.extract_links(response) if not self.is_extracted(link.url)]
+
+        # Crawl the filtered links
+        for link in next_links:
+            yield scrapy.Request(link, self.parse)
         print response.url, response.body
 
+    def is_extracted(self, url):
+        
+        id = int(url.split('/')[-2].split('-')[-1])
+        if id not in PexelsScraper.crawled_ids:
+            PexelsScraper.crawled_ids.add(id)
+            return False
+        return True
         
