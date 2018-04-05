1bc0a8f53940825d843d0f16bf97cba593e3dfa8
Denniskamau <denniskamau3@gmail.com>
2018-04-05 13:18:53 +0300

    working on a new function

--- imagecrawler/spiders/pixel.py
+++ imagecrawler/spiders/pixel.py
@@ -12,14 +12,14 @@ class PexelsScraper(scrapy.Spider):
     src_extractor = re.compile('src="([^"]*)"')
     tags_extractor = re.compile('alt="([^"]*)"')
     # Define the regex we'll need to filter the returned links
-    url_matcher = re.compile('^https:\/\/www\.textbookcentre\.com\/catalogue\/')
+    url_matcher = re.compile('^https:\/\/www\.textbookcentre\.com\/')
     
     # Create a set that'll keep track of ids we've crawled
     crawled_ids = set()
 
 
     def start_requests(self):
-        url = "https://textbookcentre.com/catalogue/category/text-books/secondary-school/"
+        url = "https://textbookcentre.com/media/products/"
         yield scrapy.Request(url, self.parse)
         
 
@@ -54,4 +54,8 @@ class PexelsScraper(scrapy.Spider):
             PexelsScraper.crawled_ids.add(id)
             return False
         return True
+        
+    def numberGenerator():
+        #should generate random numbers and append to the url and search the url if it is ok download the image there
+        #url should be like https://textbookcentre.com/media/products/2020209000111.jpg
         
